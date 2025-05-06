4f09c8c362a722df9a979642a3eef8631f652632
adamyahr <151852996+adamyahr@users.noreply.github.com>
2025-05-06 09:31:53 +0530

    Update Program_01.java

--- src/com/Test/Program_01.java
+++ src/com/Test/Program_01.java
@@ -4,7 +4,13 @@ public class Program_01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+System.out.println("Hello Word!");
+		Scanner s = new Scanner(System.in);
+	System.out.println("Enter a number:");
+		int n = s. nextInt();
+		System.out.println("square of " + n +"is:"+(n*n));
 
+	
 	}
 
 }
